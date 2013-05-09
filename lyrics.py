a222fd8966559df936d1b503a27333107f0c78dc
Student <student@ent-v321-003.local>
2013-05-08 20:09:17 -0400

    edited every .py file

---
+++ lyrics.py
@@ -0,0 +1,37 @@
+"""
+lyrics.py
+=====
+* write three programs that all do the same thing!
+* all of these programs should:
+  * print out the same four lines from your favorite song
+  * each line of lyrics should be printed out on a new line
+  * for example, if your program prints out the song, 'On a Tropical Island', 
+    the output would look like (of course, with three version, imagine this
+    three times): 
+
+On a tropical island,
+Underneath a molten lava moon.
+Hangin' with the hula dancers,
+Askin' questions cause' they got all the answers.
+
+* there are multiple ways of printing out strings on new lines:
+  * maybe it's just one command
+  * or several commands
+  * or use different ways of representing strings, putting strings 
+    together, or incorporating newlines
+* write THREE DIFFERENT versions of this program in this file 
+"""
+print("""1.
+On a tropical island,
+Underneath a molten lava moon.
+Hangin' with the hula dancers,
+Askin' questions cause' they got all the answers.""")
+print('-------------------------------------------------')
+print('2.')
+print('On a tropical island,')
+print('Underneath a molten lava moon.')
+print("Hangin' with the hula dancers,")
+print("Askin' questions cause'  they got all answers")
+print('-------------------------------------------------')
+print('3.')
+print("On a tropical island,\nUnderneath a molten lava moon.\nHangin' with the hula dancers,\nAskin' questions cause' they got all the answers.")
